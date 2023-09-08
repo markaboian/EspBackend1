113485c56132240671e747695ae380db27dab72e
markaboian <abomark09@gmail.com>
2023-09-08 23:55:44 +0300

    connection with feign and started config for rabbitmq

--- catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
+++ catalog-service/src/main/java/com/dh/catalogservice/controller/CatalogController.java
@@ -1,7 +1,9 @@
 package com.dh.catalogservice.controller;
 
 import com.dh.catalogservice.client.IMovieClient;
+import com.dh.catalogservice.client.ISerieClient;
 import com.dh.catalogservice.model.Movie;
+import com.dh.catalogservice.model.Serie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +16,9 @@ public class CatalogController {
     @Autowired
     private IMovieClient iMovieClient;
 
+    @Autowired
+    private ISerieClient iSerieClient;
+
     @GetMapping("/catalog/{genre}")
     public ResponseEntity<List<Movie>> getCatalogByGenre (@PathVariable String genre) {
         return iMovieClient.getMovieByGenre(genre);
@@ -24,4 +29,13 @@ public class CatalogController {
         return iMovieClient.saveMovie(movie);
     }
 
+    @GetMapping()
+    public ResponseEntity<List<Serie>> getSerieByGenre (@PathVariable String genre) {
+        return iSerieClient.getSerieByGenre(genre);
+    }
+
+    @PostMapping()
+    public ResponseEntity<Serie> saveSerie (@RequestBody Serie serie) {
+        return iSerieClient.saveSerie(serie);
+    }
 }
---
+++ movie-service/src/main/java/com/dh/movieservice/config/RabbitTemplateConfig.java
@@ -0,0 +1,9 @@
+package com.dh.movieservice.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitTemplateConfig {
+
+
+}
