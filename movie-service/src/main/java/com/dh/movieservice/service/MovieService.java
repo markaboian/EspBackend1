1cb1e9da1a05d7daefc53a26a6d5e745456280d7
Marianaca-29 <mariana.famaf@gmail.com>
2023-09-12 23:39:10 -0300

    rabbitMQ a movie

--- movie-service/src/main/java/com/dh/movieservice/service/MovieService.java
+++ movie-service/src/main/java/com/dh/movieservice/service/MovieService.java
@@ -3,7 +3,6 @@ package com.dh.movieservice.service;
 import com.dh.movieservice.model.Movie;
 import com.dh.movieservice.repository.MovieRepository;
 import org.springframework.stereotype.Service;
-
 import java.util.List;
 
 /**
